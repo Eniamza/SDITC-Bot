789516564a493470933b01ff5b1735338d28e5dd
Eniamza <eniamza0@gmail.com>
2021-07-03 14:35:26 +0600

    Feat: Initial Response for bot

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const prefix = config.prefix;
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+  client.user.setActivity(`Ahoy Ahoy`, { type: 'LISTENING' })
+});
+
+client.on('message', message => {
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    console.log(command);
+    
+    if(command==="ola"){
+        message.channel.send("Hukka Hua");
+
+    }
+    
+    });
+
+    client.login(config["bot-token"]);
